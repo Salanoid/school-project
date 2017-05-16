8a5c109df37f5aecdbb89743ffdf629ec42e4ec4
Salajan Silviu <salanoid@gmail.com>
2017-05-16 19:37:46 +0000

    contact php script

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
       <h1>Customer support</h1>
       <h3>Do you need help? Check if there is the question you wish to ask below. <br>If not, ask a new question with this form:</h3>
 
-      <form>
+      <form method="post" action="">
         <input type="text" name="fullname" placeholder="Full Name" size="30">
         <br><br>
         <textarea name="question" placeholder="Question" cols="28" rows="3"></textarea>
@@ -20,4 +20,18 @@
       </form>
 
     </div>
+    
+    <?php
+      if(isset($_POST["submit"])){
+        $sql = "INSERT INTO contact (name, question)
+                VALUES ('".$_POST["name"]."','".$_POST["question"]."')";
+                if ($dbh->query($sql)) {
+                  echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+                }
+                else{
+                  echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+                }
+
+                }
+?>
 <?php echo $footer; ?>
--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "salanoid";
 $password = "";
-$sql;
+
 try {
     $conn = new PDO("mysql:host=$servername;dbname=c9", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
